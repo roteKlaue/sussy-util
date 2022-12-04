bacc3c0e737d399a223b3152771e2627d1383921
roteKlaue <janoolhd@gmail.com>
2022-12-04 15:21:22 +0100

    added getContentFromDirectory

---
+++ src/Functions/getContentFromDir.ts
@@ -0,0 +1,15 @@
+import { MutableObject } from "../allInterfaces";
+import fs from "fs";
+
+const getContentFromDirectory = (dir:string) => {
+    const result = {} as MutableObject<any>;
+    fs.readdirSync(dir).forEach((path:string) => {
+        if (fs.lstatSync(`${dir}/${path}`).isDirectory()) {
+            return result[path] = getContentFromDirectory(`${dir}/${path}`);
+        }
+        result[path.replace(".js", "")] = require(`${dir}/${path}`);
+    });
+    return result;
+}
+
+export default getContentFromDirectory;
