386d77b9155f1a18584e152dc0d50872a6d99afc
roteKlaue <janoolhd@gmail.com>
2023-01-04 15:56:14 +0100

    removing function key word

--- src/Functions/getProjectRoot.ts
+++ src/Functions/getProjectRoot.ts
@@ -1,8 +1,5 @@
 import fs from "fs";
 import path from "path";
 
-const temp = (dirPath: string):string  => fs.existsSync(path.join(dirPath, "package.json"))? dirPath : temp(path.resolve(`${dirPath}/../`));
-
-export default function getProjectRoot():string {
-    return temp(__dirname);
-}
+const temp = (dirPath: string): string => fs.existsSync(path.join(dirPath, "package.json")) ? dirPath : temp(path.resolve(`${dirPath}/../`));
+export default (dir: string): string => temp(dir);
