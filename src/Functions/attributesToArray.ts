54a5fb408f8e529293fa92274a4c09267bceb4a7
roteKlaue <janoolhd@gmail.com>
2023-01-05 15:23:34 +0100

    made removeNull an optional param

--- src/Functions/attributesToArray.ts
+++ src/Functions/attributesToArray.ts
@@ -1,6 +1,6 @@
 import { MutableObject } from "../Types/";
 
-export default <T>(obj:MutableObject<T>, removeNull:boolean):T[] => {
+export default <T>(obj:MutableObject<T>, removeNull?:boolean):T[] => {
     if(!obj || typeof obj !== 'object') {
         return [];
     }
