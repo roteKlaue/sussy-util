28d085b80edc42fd92e9f3e212d0bcadf592d544
roteKlaue <janoolhd@gmail.com>
2023-01-05 15:22:47 +0100

    changed anotation

--- src/Functions/attributesToArray.ts
+++ src/Functions/attributesToArray.ts
@@ -1,6 +1,6 @@
 import { MutableObject } from "../Types/";
 
-export default (obj:MutableObject<any>, removeNull:boolean):any[] => {
+export default <T>(obj:MutableObject<T>, removeNull:boolean):T[] => {
     if(!obj || typeof obj !== 'object') {
         return [];
     }
