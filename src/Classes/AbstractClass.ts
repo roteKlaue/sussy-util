7ecfacbc9b28f60050c8eb87d66506984bb82b60
roteKlaue <janoolhd@gmail.com>
2023-01-05 22:34:29 +0100

    added documentation for AbstractClass class

--- src/Classes/AbstractClass.ts
+++ src/Classes/AbstractClass.ts
@@ -1,13 +1,16 @@
 import { AbstractClassInstanceCallError } from "../Error";
 export default class AbstractClass {
-    // TODO: Documentation
     constructor(Constructor: Function) {
         if (!AbstractClass.isClass(Constructor)) throw new TypeError("Constructor must be a class.");
         if (this.constructor == Constructor) {
+            // if constructor is the same as the instance constructor then we know that a user is trying to make a new instance of an abstract class
             throw new AbstractClassInstanceCallError("You tried to instanciate a new instance of an abstract class");
         }
     }
 
+    /**
+     * used for determining if given parameter is a constructor
+    */
     public static isClass(arg: Function) {
         if (typeof arg !== 'function') {
             return false;
